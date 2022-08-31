650b60091f36eec4266248f09a343b55c557bae4
Edsey omenda <omendajames0317@gmail.com>
2022-08-31 03:05:32 +0300

    commit tree 64a489f718188e09b2ed9f0b7aa6ff7950ce98e1

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,13 +1,4 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
   belongs_to   :bakery
-
-  def self.by_price
-    self.all.order(price: :desc)
-  end
-
-  def self.most_expensive
-    # self.all.order(price: :desc).first
-    self.all.order(price: :desc).limit(1)
-  end
 end
